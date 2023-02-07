cc525b52a072ffd831200982cad92748d59137c9
Sagar Dam <57351973+SAGAR-DAM@users.noreply.github.com>
2023-02-07 14:42:31 +0530

    Update denoising and segmentation based on histogram.py

--- Histogram based image segmentation/denoising and segmentation based on histogram.py
+++ Histogram based image segmentation/denoising and segmentation based on histogram.py
@@ -11,22 +11,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io,img_as_ubyte,img_as_float
 from skimage.restoration import denoise_nl_means, estimate_sigma
-
+from scipy import ndimage as nd
 
 image_path="D:\\Codes\\image processing\\Histogram based image segmentation\\test.jpg"
 image=img_as_float(io.imread(image_path,as_gray=True))
 
 print(image.shape)
+plt.imshow(image)
+plt.set_cmap('gray')
+plt.axis('off')
+plt.title("Main image")
+plt.show()
 
-patch_kw=dict(patch_size=5, patch_distance=6, multichannel=False)
+
+patch_kw=dict(patch_size=10, patch_distance=6, multichannel=False)
 sigma_est=np.mean(estimate_sigma(image, multichannel=False))
-denoise=denoise_nl_means(image, h=1.15*sigma_est, fast_mode=True, **patch_kw)
+denoise=denoise_nl_means(image, h=10.15*sigma_est, fast_mode=True, **patch_kw)
 
 denoise_ubyte=img_as_ubyte(denoise)
 
 plt.imshow(denoise_ubyte)
 plt.set_cmap('gray')
 plt.axis('off')
+plt.title("Denoised image")
 plt.show()
 
 
@@ -37,6 +44,28 @@ plt.show()
 
 plt.hist(denoise_ubyte.flat, bins=100, range=(0,255))
 plt.title("Histogram of the denoised image")
+plt.show()
+
+''' Segmentation of the main image without the filtering and showing the segments'''
+
+image_ubyte=img_as_ubyte(image)
+seg1=(image_ubyte <= 64)
+seg2=(image_ubyte <= 128) & (image_ubyte>64)
+seg3=(image_ubyte <=192)  & (image_ubyte>128)
+seg4=(image_ubyte>192)
+
+#print(seg1)
+
+all_segments_main_image=np.zeros((image.shape[0],image.shape[1],3))
+
+all_segments_main_image[seg1]=[1,0,0]    # showing all segments
+all_segments_main_image[seg2]=[0,1,0]
+all_segments_main_image[seg3]=[0,0,1]
+all_segments_main_image[seg4]=[1,1,0]
+
+plt.imshow(all_segments_main_image)
+plt.title("Segmented image of the main image")
+plt.axis('off')
 plt.show()
 
 segm1=(denoise_ubyte <= 64)
@@ -54,6 +83,7 @@ all_segments[segm3]=[0,0,1]
 all_segments[segm4]=[1,1,0]
 
 plt.imshow(all_segments)
+plt.title("Segmented image of the denoised image")
 plt.axis('off')
 plt.show()
 
@@ -64,6 +94,7 @@ all_segments[segm3]=[0,0,0]
 all_segments[segm4]=[0,0,0]
 
 plt.imshow(all_segments)
+plt.title("Binary image with range: 0<grayscale<=64")
 plt.axis('off')
 plt.show()
 
@@ -74,6 +105,7 @@ all_segments[segm3]=[0,0,0]
 all_segments[segm4]=[0,0,0]
 
 plt.imshow(all_segments)
+plt.title("Binary image with range: 64<grayscale<=128")
 plt.axis('off')
 plt.show()
 
@@ -84,6 +116,7 @@ all_segments[segm3]=[0,0,1]
 all_segments[segm4]=[0,0,0]
 
 plt.imshow(all_segments)
+plt.title("Binary image with range: 128<grayscale<=192")
 plt.axis('off')
 plt.show()
 
@@ -94,5 +127,35 @@ all_segments[segm3]=[0,0,0]
 all_segments[segm4]=[1,1,0]
 
 plt.imshow(all_segments)
+plt.title("Binary image with range: 192<grayscale<=255")
 plt.axis('off')
 plt.show()
+
+
+''' Filtering with scipy and segmentation of the same '''
+
+segm1_opened=nd.binary_opening(segm1, np.ones((3,3)))
+segm1_closed=nd.binary_closing(segm1_opened, np.ones((3,3)))
+
+segm2_opened=nd.binary_opening(segm2, np.ones((3,3)))
+segm2_closed=nd.binary_closing(segm2_opened, np.ones((3,3)))
+
+segm3_opened=nd.binary_opening(segm3, np.ones((3,3)))
+segm3_closed=nd.binary_closing(segm3_opened, np.ones((3,3)))
+
+segm4_opened=nd.binary_opening(segm4, np.ones((3,3)))
+segm4_closed=nd.binary_closing(segm4_opened, np.ones((3,3)))
+
+all_segments_cleaned=np.zeros((denoise_ubyte.shape[0],denoise_ubyte.shape[1],3))
+
+all_segments_cleaned[segm1]=[1,0,0]    # showing all segments
+all_segments_cleaned[segm2]=[0,1,0]
+all_segments_cleaned[segm3]=[0,0,1]
+all_segments_cleaned[segm4]=[1,1,0]
+
+plt.imshow(all_segments_cleaned)
+plt.axis('off')
+plt.title("Segmented image after scipy filtering")
+plt.show()
+
+
